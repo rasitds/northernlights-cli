9653616e38936ac64f4403127fb15936581308d5
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2018-01-28 04:23:09 +0300

    Add search and analyze commands

--- src/command.cpp
+++ src/command.cpp
@@ -1,7 +1,7 @@
 #include "command.hpp"
 #include "db.hpp"
 #include "utils.hpp"
-#include <iostream>
+
 Command::Command() {
     addCommand({ "admin", {},
         [](std::queue<std::string>&) {
@@ -32,6 +32,45 @@ Command::Command() {
             text += params.back();
 
             print(text);
+        }
+    });
+    addCommand({ "search", {},
+        [](std::queue<std::string>& params) {
+            print("Searching...", "response");
+            bool searchStatus = false;
+
+            std::string text;
+
+            for (int j = 0; j <= params.size() + 1; j++) {
+                text += params.front() + " ";
+
+                if (!params.empty())
+                    params.pop();
+            }
+
+            text += params.back();
+
+            //...
+
+            if (!searchStatus)
+                print("NO VALID SEARCH FOUND.", "response");
+            else
+                print("SHOWING RESULTS", "response");
+        }
+    });
+    addCommand({ "analyze", {},
+        [](std::queue<std::string>&) {
+            print("Analyzing...", "response");
+            bool resultFound = false;
+
+            //...
+
+            if (!resultFound)
+                print("NO RESULT FOUND.", "response");
+            else
+                print("Results:");
+
+            //...
         }
     });
 }
