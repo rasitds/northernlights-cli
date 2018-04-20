bc9ee05533b770e1c324a2d516011e01265b4625
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2018-04-20 10:11:53 +0300

    Trying nested loops 0001

--- src/system.cpp
+++ src/system.cpp
@@ -19,8 +19,16 @@ void System::boot()
     {
     case 0:
         {
+            int num = 1;
             std::string nlString = "Northern Lights";
             std::cout << &nlString << std::endl;
+            for (int row = 1; row <= 7; row++) {
+                for (int column = 0; column <= row; column++) {
+                    num++;
+                    std::cout << num << " ";
+                }
+                std::cout << row << "\n" << num << " ";
+            }
         }
     case 1:
     case 10:
