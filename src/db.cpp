08d78dc88ea941927f717cbf575b806449e78b5f
durmusrasit <12167194+durmusrasit@users.noreply.github.com>
2018-04-24 03:32:44 +0300

    Add database init system messages

--- src/db.cpp
+++ src/db.cpp
@@ -11,13 +11,19 @@ DB::~DB() {
 
 void DB::init() {
     print("system", "INIT: DATABASE");
+    print("system", "RUN: INITIALIZING DATABASE");
     load();
 }
 
 void DB::load() {
+    print("system", "RUN: DATABASE LOADING..");
+
+    print("system", "RUN: READ DATABASE FILE");
     FILE* db_file = fopen("db.json", "r");
 
+    print("system", "RUN: CHECK DATABASE FILE");
     if (!db_file) {
+        print("system", "RUN: WRITE DATABASE FILE");
         db_file = fopen("db.json", "w");
         fputs("{}", db_file);
         fclose(db_file);
